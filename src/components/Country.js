363b97ceae6c0f7db6b19d73ee9f960bc506af0a
unknown <josiptadic96@hotmail.com>
2022-07-23 14:16:47 +0200

    2.14*: Data for countries, step3

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [weatherData, setWeatherData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+          country.capital[0] +
+          "&appid=" +
+          process.env.REACT_APP_WEATHER_API_KEY
+      )
+      .then((response) => {
+        setWeatherData(response.data);
+      });
+  }, [country]);
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -13,6 +28,19 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt="country flag" />
+      <h2>Weather in {country.capital[0]}</h2>
+      {weatherData && (
+        <>
+          <p>
+            temperature {(weatherData.main.temp - 273.15).toFixed(2)} Celsius
+          </p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+            alt="wheather icon"
+          />
+          <p>wind {weatherData.wind.speed} m/s</p>
+        </>
+      )}
     </div>
   );
 };
