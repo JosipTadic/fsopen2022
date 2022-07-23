e8d634b3ef516f7c446dcc0c1bab41c72ce3d9f7
unknown <josiptadic96@hotmail.com>
2022-07-23 13:05:24 +0200

    2.13*: Data for countries, step2

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,46 +4,30 @@ import Country from "./Country";
 import CountryList from "./CountryList";
 
 const Countries = ({ nameFilter }) => {
+  const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
     });
   }, []);
 
-  const [countries, setCountries] = useState([]);
-
-  const renderCountries = (noOfCountries) => {
-    if (noOfCountries > 10) {
-      return <p>Too many matches, specify another filter</p>;
-    } else if (noOfCountries < 10 && noOfCountries > 1) {
-      return countries
-        .filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        )
-        .map((country) => (
-          <CountryList
-            key={country.name.common}
-            countryName={country.name.common}
-          />
-        ));
-    } else {
-      return countries
-        .filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        )
-        .map((country) => (
-          <Country key={country.name.common} country={country} />
-        ));
-    }
-  };
-  return (
-    <>
-      {renderCountries(
-        countries.filter((country) =>
-          country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
-        ).length
-      )}
-    </>
+  const countryMatches = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(nameFilter.toLowerCase())
   );
+
+  const noOfCountries = countryMatches.length;
+
+  if (noOfCountries > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (noOfCountries < 10 && noOfCountries > 1) {
+    return countryMatches.map((country) => (
+      <CountryList key={country.name.common} country={country} />
+    ));
+  } else {
+    return countryMatches.map((country) => (
+      <Country key={country.name.common} country={country} />
+    ));
+  }
 };
 export default Countries;
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Country from "./Country";
 
-const CountryList = ({ countryName }) => {
-  return <p>{countryName}</p>;
+const CountryList = ({ country }) => {
+  const [showCountry, setShowCountry] = useState(false);
+
+  return (
+    <>
+      <p>
+        {country.name.common}
+        <button onClick={() => setShowCountry(!showCountry)}>
+          {showCountry ? "hide" : "show"}
+        </button>
+      </p>
+      {showCountry && <Country country={country} />}
+    </>
+  );
 };
 
 export default CountryList;
