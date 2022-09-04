820d406f24008fde49980d3febe4baf12d4858de
JOSP <josiptadic96@hotmail.com>
2022-09-04 14:01:46 +0200

    3.11 phonebook full stack

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,34 +1,23 @@
 import axios from "axios";
 const personsUrl = "/api/persons";
-const personUrl = "/api/person";
 
 const getAll = () => {
-  const request = axios.get(
-    `${process.env.REACT_APP_PHONEBOOK_BASE_URL + personsUrl}`
-  );
+  const request = axios.get(`${personsUrl}`);
   return request.then((response) => response.data);
 };
 
 const create = (newPerson) => {
-  const request = axios.post(
-    `${process.env.REACT_APP_PHONEBOOK_BASE_URL + personsUrl}`,
-    newPerson
-  );
+  const request = axios.post(`${personsUrl}`, newPerson);
   return request.then((response) => response.data);
 };
 
 const deletePerson = (id) => {
-  const request = axios.delete(
-    `${process.env.REACT_APP_PHONEBOOK_BASE_URL + personUrl}/${id}`
-  );
+  const request = axios.delete(`${personsUrl}/${id}`);
   return request.then((response) => response.data);
 };
 
 const updatePerson = (id, phoneNumber) => {
-  const request = axios.put(
-    `${process.env.REACT_APP_PHONEBOOK_BASE_URL + personUrl}/${id}`,
-    phoneNumber
-  );
+  const request = axios.put(`${personsUrl}/${id}`, phoneNumber);
   return request.then((response) => response.data);
 };
 
